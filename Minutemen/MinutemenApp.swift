e8ad1f08289fa00e320a1283e48e2b47f04d4d00
Eugène KIRILLOV <e.kirillov@betclicgroup.com>
2022-10-07 10:46:07 +0200

    Initial Commit

---
+++ Minutemen/MinutemenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinutemenApp.swift
+//  Minutemen
+//
+//  Created by Eugene KIRILLOV on 07/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MinutemenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
